365af9649556f3599aba14531fa403534a4ba3c3
torsah199 <116138935+torsah199@users.noreply.github.com>
2022-10-19 11:12:44 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onGesture(Gesture.Shake, function () {
+    let Random = 0
+    let _2 = 0
+    basic.clearScreen()
+    if (Random == _2) {
+        basic.showString("Yes")
+    } else {
+        let _1 = 0
+        if (Random == _1) {
+            basic.showString("NO")
+        } else {
+            basic.showString("I DON'T KNON")
+        }
+    }
+    basic.showNumber(8)
+})
+basic.showString("Hello!")
+basic.showNumber(0)
